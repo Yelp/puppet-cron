380c0af5bbeecd0c7b7d16d8e1a591b9eeaaed3a
Josh Snyder <josnyder@yelp.com>
2014-04-30 18:55:47 -0700

    Purge

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,11 @@
 class cron {
   file { '/nail/etc/cron.d':
-    ensure => 'directory',
-    mode   => '0755',
-    owner  => 'root',
-    group  => 'root',
+    ensure  => 'directory',
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    recurse => true,
+    purge   => true,
+    force   => true,
   }
 }
