abce296a0aad52082d5b2da902010932b1458920
Josh Snyder <josnyder@yelp.com>
2014-07-07 18:44:18 -0700

    whitespace

--- manifests/d.pp
+++ manifests/d.pp
@@ -39,51 +39,51 @@
 #
 
 define cron::d (
-                    $minute,
-                    $command,
-                    $user,
-                    $hour='*',
-                    $dom='*',
-                    $month='*',
-                    $dow='*',
-                    $mailto='""',
-                    $log_to_syslog=true,
-                    $staleness_threshold=undef,
-                    $staleness_check_params=undef,
-                    $comment=''
-                ) {
-    # Deliberate copy here so we can add extra fancy options (like pipe stdout
-    # to scribe) in additional parameters later
-    validate_cron_numeric($minute)
-    validate_cron_numeric($hour)
-    validate_cron_numeric($dom)
-    validate_cron_numeric($month)
-    validate_cron_numeric($dow)
+  $minute,
+  $command,
+  $user,
+  $hour='*',
+  $dom='*',
+  $month='*',
+  $dow='*',
+  $mailto='""',
+  $log_to_syslog=true,
+  $staleness_threshold=undef,
+  $staleness_check_params=undef,
+  $comment=''
+) {
+  # Deliberate copy here so we can add extra fancy options (like pipe stdout
+  # to scribe) in additional parameters later
+  validate_cron_numeric($minute)
+  validate_cron_numeric($hour)
+  validate_cron_numeric($dom)
+  validate_cron_numeric($month)
+  validate_cron_numeric($dow)
 
-    validate_bool($log_to_syslog)
+  validate_bool($log_to_syslog)
 
-    require cron
+  require cron
 
-    $reporting_name = "cron_${name}"
+  $reporting_name = "cron_${name}"
 
-    if $staleness_threshold {
-      $actual_command = "/nail/sys/bin/success_wrapper '${reporting_name}' ${command}"
+  if $staleness_threshold {
+    $actual_command = "/nail/sys/bin/success_wrapper '${reporting_name}' ${command}"
 
-      cron::staleness_check { $reporting_name:
-        threshold => $staleness_threshold,
-        params    => $staleness_check_params,
-        user      => $user,
-      }
-    } else {
-      $actual_command = $command
+    cron::staleness_check { $reporting_name:
+      threshold => $staleness_threshold,
+      params    => $staleness_check_params,
+      user      => $user,
     }
+  } else {
+    $actual_command = $command
+  }
 
-    # If both syslogging and mailing are requested, choose mailing over syslogging
-    $actually_log_to_syslog = $log_to_syslog and $mailto=='""'
+  # If both syslogging and mailing are requested, choose mailing over syslogging
+  $actually_log_to_syslog = $log_to_syslog and $mailto=='""'
 
-    cron::file { $name:
-      file_params => {
-        content => template('cron/d.erb'),
-      },
-    }
+  cron::file { $name:
+    file_params => {
+      content => template('cron/d.erb'),
+    },
+  }
 }
