217d087fc410e4b13fa6683fe55afbfe5aa83451
Sophie Matthews <smatthew@yelp.com>
2019-09-10 04:00:18 -0700

    Allow us to toggle whether cron should be ensured to be running or not

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -13,4 +13,18 @@ describe 'cron' do
    it { should contain_file('/nail/sys/bin/cron_staleness_check') }
   end
 
+  context 'with service ensure stopped' do
+    let(:facts) {{
+      :lsbdistid => 'Ubuntu',
+      :lsbdistrelease => '16.04',
+      :operatingsystemrelease => '16.04',
+      :osfamily => 'Debian',
+    }}
+    let(:params) {{
+      :service_ensure => 'stopped'
+    }}
+    it { should compile }
+    it { should contain_service('cron').with_ensure('stopped') }
+  end
+
 end
